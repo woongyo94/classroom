7d64d8dcc46f0e4cfbcd702fa295dcedd2aee1bf
woongyo94 <109453599+woongyo94@users.noreply.github.com>
2022-10-04 17:25:51 +0900

    Update main.py

--- main.py
+++ main.py
@@ -9,17 +9,24 @@ class Ball:
         self.id = canvas.create_oval(10, 10, 25, 25, fill=color)
         self.canvas.move(self.id, 245, 100)
 
-        self.x = 0
-        self.y = -1
+        starts = [ -3, -2, -1, 1, 2, 3 ]
+        random.shuffle(starts)
+        self.x = starts[0]
+        self.y = -3
         self.canvas_height = self.canvas.winfo_height()
+        self.canvas_width = self.canvas.winfo_width()
 
     def draw(self):
         self.canvas.move(self.id, self.x, self.y)
         pos = self.canvas.coords(self.id)
         if pos[1] <= 0 :
-            self.y = 1
+            self.y = 3
         if pos[3] >= self.canvas_height:
-            self.y = -1
+            self.y = -3
+        if pos[0] <= 0 :
+            self.x = 3
+        if pos[2] >= self.canvas_width :
+            self.x = -3
 
 tk = Tk()
 tk.title("Game")
